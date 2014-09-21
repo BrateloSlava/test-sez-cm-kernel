adfeff0a966812d174f030595ff62582c64c2c6a
Jake Weinstein <xboxlover360@gmail.com>
2014-09-21 18:23:00 -0500

    PM_QoS: add back comments to header
    Conflicts:
    	include/linux/pm_qos.h

--- include/linux/pm_qos.h
+++ include/linux/pm_qos.h
@@ -192,10 +192,6 @@ struct dev_pm_qos {
 	struct dev_pm_qos_request *flags_req;
 };
 
-struct dev_pm_qos {
-	struct pm_qos_constraints latency;
-};
-
 /* Action requested to pm_qos_update_target */
 enum pm_qos_req_action {
 	PM_QOS_ADD_REQ,		/* Add a new request */
@@ -205,7 +201,7 @@ enum pm_qos_req_action {
 
 static inline int dev_pm_qos_request_active(struct dev_pm_qos_request *req)
 {
-	return req->dev != 0;
+	return req->dev != NULL;
 }
 
 int pm_qos_update_target(struct pm_qos_constraints *c, struct plist_node *node,
